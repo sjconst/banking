d1f655aa721fa23867cf8368fbe2b8df5d4850a4
sjconst <stephanie.c.lake@gmail.com>
2019-12-13 15:22:42 -0800

    added form to modal

--- client/src/components/signup/index.js
+++ client/src/components/signup/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.css";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Form } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { CLOSE } from "../../actions";
 
@@ -16,7 +16,23 @@ function Signup(){
                         <Modal.Title>Sign up now!</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <p>Test.</p>
+                        <Form>
+                            <Form.Group>
+                                <Form.Label>First Name</Form.Label>
+                                <Form.Control type="text" placeholder="Enter first name"/>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Last Name</Form.Label>
+                                <Form.Control type="text" placeholder="Enter last name"/>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="Enter first name"/>
+                                <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+                        </Form>
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={close}>Close</Button>               
